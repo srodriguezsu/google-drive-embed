226fef06800b1764e68eb7e5fa3d1e4294a9a4e9
Sebastian Rodriguez Suarez <129565458+srodriguezsu@users.noreply.github.com>
2025-07-10 14:41:46 -0500

    attempt to integrate with elementor

--- google-drive-embed.php
+++ google-drive-embed.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Insertar Google Drive
 Description: Inserta archivos o carpetas de Google Drive al editor de WordPress.
-Version: 1.2.2
+Version: 1.2.3
 Author: Sebastian Rodriguez
 */
 
